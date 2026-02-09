25051a48f05c2162e26acb28db553ca948c158b7
IRMAK\irmakgenc <irmak.genc@outlook.com>
2026-02-09 14:15:46 +0300

    chore: initialize project

---
+++ src/test/java/com/irmakgenc/paymentrouting/PaymentRoutingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irmakgenc.paymentrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRoutingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
